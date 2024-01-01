663ff3069413ffd0e0a44600bdf6bd2208f8259b
tjrelyts <tylerj-santo@outlook.com>
2023-12-31 19:19:57 -0500

    Fixed error regarding Portfolio.tsx.

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,9 +1,9 @@
 export default [
-  /*{
+  {
     title: "Dave's Book Emporium",
     imgURL: "https://placehold.co/512x256",
     stack: ["SQL"],
     link: "https://drive.google.com/drive/folders/1SkYXf35wzxxc76SCFzEjMN4CdBshciV4?usp=drive_link",
     text: "A representation of an online bookstore created as a final project for my Database Systems class. This project required teamwork, communication, presentation skills, and SQL knowledge to create. ",
-  },*/
+  },
 ];
