fcc5bf2c6e0362d3c4222e25e0b9c85e2243b270
tjrelyts <tylerj-santo@outlook.com>
2024-03-04 22:40:04 -0500

    Edited the format of portfolio section. Updated portfolio items.

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -14,10 +14,13 @@ const Portfolio = () => {
         This is a collection of some work I have contributed to or have created
         myself.
       </p>
-      <div className="overflow-hidden mt-10">
-        <ul className="flex flex-wrap gap-20 justify-center">
-          {portfolio.map((project) => (
-            <li className="">
+      <div className="overflow-hidden mt-10 w-full">
+        <ul className="flex flex-wrap gap-20 justify-center m-10">
+          {portfolio.map((project, index) => (
+            <li
+              key={index}
+              className="hover:scale-110 transform transition-transform duration-500 ease-in-out"
+            >
               <PortfolioItem
                 imgURL={project.imgURL}
                 title={project.title}
--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -9,23 +9,12 @@ interface Props {
 const PortfolioItem = ({ title, imgURL, stack, link, text }: Props) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" className="group">
-      <div className="relative w-[512px] h-[256px]">
+      <div className="relative w-[480px] h-[240px]">
         <img
           src={imgURL}
           className="object-cover w-full h-full rounded-lg"
         ></img>
-        <div className="absolute h-0 w-0 left-0 bottom-0 right-0 top-0 m-auto rounded-lg group-hover:h-full group-hover:w-full transition-all duration-500 ease-out bg-black bg-opacity-50 opacity-0 group-hover:opacity-100">
-          {" "}
-          <p className="flex justify-center items-center w-full h-full flex-wrap">
-            {text}
-            {stack.map((item) => (
-              <span className="inline-block px-2 font-bold text-lg border-2 border-black rounded-md ">
-                {item}
-                {title}
-              </span>
-            ))}
-          </p>
-        </div>
+        <div className="absolute bg-black opacity-0 w-full h-full top-0 rounded-lg group-hover:opacity-75 duration-500"></div>
       </div>
     </a>
   );
