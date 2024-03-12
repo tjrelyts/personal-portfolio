7389c87b5de288680c43fa98e9d6624dabc36b3e
tjrelyts <tylerj-santo@outlook.com>
2024-03-12 14:16:42 -0400

    Image fix.

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,21 +1,21 @@
 export default [
   {
     title: "Dave's Book Emporium",
-    imgURL: "/assets/dave.jpg",
+    imgURL: "../src/assets/dave.jpg",
     stack: ["SQL"],
     link: "https://drive.google.com/drive/folders/1SkYXf35wzxxc76SCFzEjMN4CdBshciV4?usp=drive_link",
     text: "A representation of an online bookstore created as a final project for my Database Systems class. This project required teamwork, communication, presentation skills, and SQL knowledge to create. ",
   },
   {
     title: "M31",
-    imgURL: "/assets/m31.jpg",
+    imgURL: "../src/assets/m31.jpg",
     stack: ["Java"],
     link: "https://github.com/tjrelyts/M31-2D-Solar-Simulator",
     text: "A 2D solar system simulator that makes use of the LibGDX framework. Incorporates orbital mechanics with realistic values to provide exact scale.",
   },
   {
     title: "Sentiment Analyzer",
-    imgURL: "/assets/sentiment.jpg",
+    imgURL: "../src/assets/sentiment.jpg",
     stack: ["Python"],
     link: "https://github.com/tjrelyts/Sentiment-Analysis",
     text: "A machine learning model to determine the positive or negative sentiment of a statement.",
